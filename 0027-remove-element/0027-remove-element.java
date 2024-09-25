aad7f949f2fe351a11d7f53582f09464e419fc47
Madhusudan Singh Chauhan <136247818+Madhusudan0012@users.noreply.github.com>
2024-09-26 03:25:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (32.01%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // Pointer to track the position of the non-val elements
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i]; // Replace the element at k with nums[i]
+        int k = 0 ;
+        for(int i = 0 ; i<nums.length ; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
                 k++;
             }
         }
-        
-        return k; // The new length of the array with val removed
+        return k;
+
     }
 }
